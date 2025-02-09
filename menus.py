f2143605b22b4a255dd63db823ecef79a25134d6
Vitaliy Golubenko <checkybox@proton.me>
2025-02-09 19:38:04 +0500

    new game over menu

--- menus.py
+++ menus.py
@@ -24,17 +24,25 @@ def draw_button(text, x, y, width, height, screen, font):
     draw_text(text, font, TEXT_COLOR, screen, x + width // 2, y + height // 2)
     return button_rect
 
-def settings_menu(screen, game, SCREEN_WIDTH):
+def settings_menu(screen, game, SCREEN_WIDTH, GAME_UPDATE):
     running = True
     while running:
         screen.fill(Colors.dark_blue)
-        draw_text("Settings", Fonts.press_2p(120), Colors.white, screen, SCREEN_WIDTH // 2, 300)
+        draw_text("SETTINGS", Fonts.press_2p(110), Colors.white, screen, SCREEN_WIDTH // 2, 300)
 
         # draw buttons
         if game.is_music_on:
             music_toggle = draw_button("Music:ON", 350 - 50, 450, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
         else:
             music_toggle = draw_button("Music:OFF", 350 - 50, 450, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
+
+        if game.difficulty == 0:
+            difficulty_toggle = draw_button("Mode:Easy", 350 - 50, 550, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
+        elif game.difficulty == 1:
+            difficulty_toggle = draw_button("Mode:Medium", 350 - 100, 550, BUTTON_WIDTH + 200, BUTTON_HEIGHT, screen,Fonts.press_2p(40))
+        elif game.difficulty == 2:
+            difficulty_toggle = draw_button("Mode:Hard", 350 - 50, 550, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen,Fonts.press_2p(40))
+
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -42,12 +50,19 @@ def settings_menu(screen, game, SCREEN_WIDTH):
                 game.save_score(game.score)
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and music_toggle.collidepoint(event.pos):
-                game.toggle_music()
+
+            # exit settings on escape press
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
 
-def pause_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH, paused):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if music_toggle.collidepoint(event.pos):
+                    game.toggle_music()
+                elif difficulty_toggle.collidepoint(event.pos):
+                    new_difficulty_ms = game.toggle_difficulty()
+                    pygame.time.set_timer(GAME_UPDATE, new_difficulty_ms)
+
+def pause_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH, GAME_UPDATE, paused):
     bounce_offset = 0
     bounce_direction = 15
 
@@ -78,7 +93,7 @@ def pause_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH, paused):
                 if continue_button.collidepoint(event.pos):
                     paused = False
                 elif settings_button.collidepoint(event.pos):
-                    settings_menu(screen, game, SCREEN_WIDTH)
+                    settings_menu(screen, game, SCREEN_WIDTH, GAME_UPDATE)
                 elif help_button.collidepoint(event.pos):
                     print("Help menu placeholder")
                 elif quit_button.collidepoint(event.pos):
@@ -94,7 +109,7 @@ def pause_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH, paused):
                     bounce_direction = 15
                 bounce_offset += bounce_direction
 
-def main_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH):
+def main_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH, GAME_UPDATE):
     bounce_offset = 0
     bounce_direction = 15
 
@@ -103,8 +118,7 @@ def main_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH):
         draw_text("TETRIS", Fonts.press_2p(120), Colors.white, screen, 500, 300 + bounce_offset)
         play_button = draw_button("Play", 350 - 50, 450, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
         settings_button = draw_button("Settings", 350 - 50, 550, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
-        help_button = draw_button("Help", 350 - 50, 650, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
-        quit_button = draw_button("Quit", 350 - 50, 750, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
+        quit_button = draw_button("Quit", 350 - 50, 650, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
 
         pygame.display.update()
 
@@ -119,9 +133,7 @@ def main_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH):
                 if play_button.collidepoint(event.pos):
                     return
                 elif settings_button.collidepoint(event.pos):
-                    settings_menu(screen, game, SCREEN_WIDTH)
-                elif help_button.collidepoint(event.pos):
-                    print("Help menu placeholder")
+                    settings_menu(screen, game, SCREEN_WIDTH, GAME_UPDATE)
                 elif quit_button.collidepoint(event.pos):
                     game.save_score(game.score)
                     pygame.quit()
@@ -134,3 +146,30 @@ def main_menu(screen, game, BOUNCE_UPDATE, SCREEN_WIDTH):
                 elif bounce_offset <= -15:
                     bounce_direction = 15
                 bounce_offset += bounce_direction
+
+def game_over_menu(screen, game, BUTTON_WIDTH, BUTTON_HEIGHT, SCREEN_WIDTH):
+    while game.game_over:
+        screen.fill(Colors.dark_blue)
+        draw_text("GAME OVER", Fonts.press_2p(100), Colors.white, screen, SCREEN_WIDTH // 2, 300)
+        draw_text("SCORE:", Fonts.press_2p(60), Colors.white, screen, SCREEN_WIDTH // 2 - 150, 400)
+        draw_text(f"{game.score}", Fonts.press_2p(40), Colors.white, screen, SCREEN_WIDTH // 2 + 150, 400)
+        restart_button = draw_button("Restart", 350 - 50, 450, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
+        quit_button = draw_button("Quit", 350 - 50, 550, BUTTON_WIDTH + 100, BUTTON_HEIGHT, screen, Fonts.press_2p(40))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game.save_score()
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if restart_button.collidepoint(event.pos):
+                    game.game_over = False
+                    game.reset()
+                    return
+
+                elif quit_button.collidepoint(event.pos):
+                    game.save_score(game.score)
+                    pygame.quit()
+                    sys.exit()

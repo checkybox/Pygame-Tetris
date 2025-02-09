fc5a79f1e10fc53988fe5c9207e6ac37687c4853
Vitaliy Golubenko <checkybox@proton.me>
2025-02-10 00:27:34 +0500

    add default size value for returned fonts

--- fonts.py
+++ fonts.py
@@ -1,11 +1,13 @@
 import pygame, os
 
 class Fonts:
+    #returns basic font with specified size
     @classmethod
     def basic(cls, size):
-        return pygame.font.Font(None, size)
+        return pygame.font.Font(None, size = 40)
 
+    # returns press_2p font with specified size
     @classmethod
-    def press_2p(cls, size):
+    def press_2p(cls, size = 40):
         cwd = os.getcwd()
         return pygame.font.Font(f"{cwd}/assets/fonts/Press_Start_2P/PressStart2P-Regular.ttf", size)
